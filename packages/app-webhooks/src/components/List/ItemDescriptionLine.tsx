7b079dc368dc3f253c64d88e61c7788fc39c4596
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-01-18 10:35:24 +0100

    chore: cleanup DescriptionLine replacing export related stuff

--- packages/app-webhooks/src/components/List/ItemDescriptionLine.tsx
+++ packages/app-webhooks/src/components/List/ItemDescriptionLine.tsx
@@ -1,24 +1,17 @@
-import { formatDate } from '@commercelayer/core-app-elements'
-import { Export } from '@commercelayer/sdk'
+import { Webhook } from '@commercelayer/sdk'
 
 interface Props {
-  job: Export
+  job: Webhook
 }
 
 export function DescriptionLine({ job }: Props): JSX.Element {
   return (
     <>
-      {job.status === 'pending' ? (
-        <div>Pending</div>
-      ) : job.status === 'in_progress' ? (
-        <div>In progress</div>
-      ) : job.interrupted_at != null ? (
-        <div>Export failed on {formatDate(job.interrupted_at)}</div>
-      ) : job.status === 'completed' ? (
-        <div>Exported on {formatDate(job.completed_at)}</div>
-      ) : (
-        '-'
-      )}
+      {job.circuit_state === 'open'
+        ? '-'
+        : job.circuit_state === 'closed'
+        ? '-'
+        : '-'}
     </>
   )
 }
