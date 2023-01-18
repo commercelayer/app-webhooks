85311d578b68ba0acc435a90ca981af9e177448a
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-01-18 10:34:53 +0100

    fix: fix utils test suite replacing export related stuff

--- packages/app-webhooks/src/components/List/utils.test.ts
+++ packages/app-webhooks/src/components/List/utils.test.ts
@@ -2,25 +2,11 @@ import { getUiStatus } from './utils'
 
 // getUiStatus
 describe('getUiStatus', () => {
-  test('should return `progress` status for the `<StatusIcon>` component, when job is `in_progress`', () => {
-    expect(getUiStatus('in_progress')).toBe('progress')
+  test('should return `danger` status for the `<StatusIcon>` component when job is `open`', () => {
+    expect(getUiStatus('open')).toBe('danger')
   })
 
-  test('should return `danger` status for the `<StatusIcon>` component when job is `interrupted`', () => {
-    expect(getUiStatus('interrupted')).toBe('danger')
-  })
-
-  test('should return `success` status for the `<StatusIcon>` component when job is `completed`', () => {
-    expect(getUiStatus('completed')).toBe('success')
-  })
-
-  test('should return `pending` status for the `<StatusIcon>` component when job is `pending`', () => {
-    expect(getUiStatus('pending')).toBe('pending')
-  })
-
-  test('should return `pending` status for the `<StatusIcon>` component when job is unknown', () => {
-    expect(getUiStatus('')).toBe('pending')
-    expect(getUiStatus(undefined)).toBe('pending')
-    expect(getUiStatus('some-not-recognized-text')).toBe('pending')
+  test('should return `success` status for the `<StatusIcon>` component when job is `closed`', () => {
+    expect(getUiStatus('closed')).toBe('success')
   })
 })
