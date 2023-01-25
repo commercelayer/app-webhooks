733a42bab6d772e3d7c56470deb87fc42963e222
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-01-25 10:17:10 +0100

    chore: replace javascript-time-ago with date-fns in utils/timeAgo

--- packages/app-webhooks/src/utils/timeAgo.ts
+++ packages/app-webhooks/src/utils/timeAgo.ts
@@ -1,15 +1,11 @@
-import TimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
-TimeAgo.addDefaultLocale(en)
+import formatDistance from 'date-fns/formatDistance'
 
 /**
- * Get the "time ago" style string comparing actual datetime to the one requested.
- * @param dateTime - A string containing a datetime (typically in format 0000-00-00T00:00:00).
- * @returns a string containing the wanted "time ago" string (eg. Just now, 1 minute, 1 hour ago).
+ * Get the "time ago" style string starting from a requested datetime string.
+ * @param dateTime - A string containing a parsable datetime.
+ * @returns a string containing a "XXX ago" string (eg. about 1 minute ago, about 1 hour ago).
  */
 export function getTimeAgoString(dateTime: string): string {
-  const timeAgo = new TimeAgo('en-US')
   const dateTimeToDate = Date.parse(dateTime)
-  const diffMilliseconds = Date.now() - dateTimeToDate
-  return timeAgo.format(Date.now() - diffMilliseconds)
+  return `${formatDistance(Date.now(), dateTimeToDate)} ago`
 }
