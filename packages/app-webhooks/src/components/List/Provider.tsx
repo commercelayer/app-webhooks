da92eb450f1b5dbbb73c118ed3c6849821a6116b
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-01-25 11:54:22 +0100

    chore: update List provider to fetch event_callbacks through sdk include instead of nested requests

--- packages/app-webhooks/src/components/List/Provider.tsx
+++ packages/app-webhooks/src/components/List/Provider.tsx
@@ -1,4 +1,4 @@
-import { CommerceLayerClient, Webhook, EventCallback } from '@commercelayer/sdk'
+import { CommerceLayerClient, Webhook } from '@commercelayer/sdk'
 import { ListResponse } from '@commercelayer/sdk/lib/cjs/resource'
 import { ListWebhookContextValue, ListWebhookContextState } from 'App'
 import {
@@ -47,16 +47,6 @@ export function ListWebhookProvider({
           state,
           pageSize
         })
-        for (const webhook of list) {
-          const lastEventCallbacks = await getWebhookLastEventCallbacks({
-            cl: sdkClient,
-            webhookId: webhook.id
-          })
-          const webhookIndex = list?.findIndex((w) => w.id === webhook.id)
-          if (list[webhookIndex] !== undefined) {
-            list[webhookIndex].last_event_callbacks = lastEventCallbacks
-          }
-        }
         dispatch({ type: 'setList', payload: list })
       } finally {
         handleLoadingState && dispatch({ type: 'setLoading', payload: false })
@@ -144,18 +134,9 @@ const getAllWebhooks = async ({
   return await cl.webhooks.list({
     pageNumber: state.currentPage,
     pageSize,
-    sort: state.sort
+    sort: state.sort,
+    include: ['last_event_callbacks']
   })
-}
-
-const getWebhookLastEventCallbacks = async ({
-  cl,
-  webhookId
-}: {
-  cl: CommerceLayerClient
-  webhookId: string
-}): Promise<ListResponse<EventCallback>> => {
-  return await cl.webhooks.last_event_callbacks(webhookId)
 }
 
 const statusForPolling: Array<Webhook['circuit_state']> = ['closed', 'open']
