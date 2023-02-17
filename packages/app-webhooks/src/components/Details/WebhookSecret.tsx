a99b8326a23beec9597937a44d7fd46fa36ad8fd
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-02-17 12:12:56 +0100

    chore: adapt some components with new props provided by upgraded library

--- packages/app-webhooks/src/components/Details/WebhookSecret.tsx
+++ packages/app-webhooks/src/components/Details/WebhookSecret.tsx
@@ -1,10 +1,4 @@
-import {
-  A,
-  InputReadonly,
-  ListDetails,
-  Spacer,
-  Text
-} from '@commercelayer/core-app-elements'
+import { A, InputReadonly, ListDetails } from '@commercelayer/core-app-elements'
 import { useWebhookDetailsContext } from './Provider'
 
 export function WebhookSecret(): JSX.Element | null {
@@ -18,22 +12,24 @@ export function WebhookSecret(): JSX.Element | null {
 
   return (
     <ListDetails title='Shared secret'>
-      <Spacer bottom='4'>
-        <hr />
-      </Spacer>
-      <Spacer bottom='4'>
-        <Text variant='info'>
-          Used to sign the payload. Learn how to{' '}
-          <A
-            href='https://docs.commercelayer.io/core/callbacks-security'
-            target='_blank'
-          >
-            verify the callback authenticity
-          </A>
-          .
-        </Text>
-      </Spacer>
-      <InputReadonly value={data.shared_secret} showCopyAction />
+      <InputReadonly
+        value={data.shared_secret}
+        showCopyAction
+        hint={{
+          text: (
+            <>
+              Used to sign the payload. Learn how to{' '}
+              <A
+                href='https://docs.commercelayer.io/core/callbacks-security'
+                target='_blank'
+              >
+                verify the callback authenticity
+              </A>
+              .
+            </>
+          )
+        }}
+      />
     </ListDetails>
   )
 }
