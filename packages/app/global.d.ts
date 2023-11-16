5a0ab41b9bbffa531004f259b0c8b7442fd9c78c
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-11-16 17:41:19 +0100

    docs: add ts doc to window.clAppConfig type

--- packages/app/global.d.ts
+++ packages/app/global.d.ts
@@ -3,7 +3,14 @@ export {}
 declare global {
   interface Window {
     clAppConfig: {
+      /**
+       * Specific domain to use for Commerce Layer API requests.
+       * It must be set as `commercelayer.io`.
+       */
       domain: string
+      /**
+       * Enable Google Tag Manager for the provided GTM ID.
+       */
       gtmId: string
     }
   }
